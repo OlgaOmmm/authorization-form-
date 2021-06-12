a3241a029be825b099d401d61986caed3f19196e
Olga <53094440+OlgaOmmm@users.noreply.github.com>
2021-06-13 00:48:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var cfml = {}
+var cfml = {};
 
-cfml.authForm = document.getElementById('AuthForm')
-cfml.authBT = document.getElementById('AuthBT')
-cfmluserLogin = document.getElementById('LoginInput')
-cfml.userPassword = document.getElementById('PasswordInput')
+cfml.authForm = document.getElementById('AuthForm');
+cfml.authBT = document.getElementById('AuthBT');
+cfmluserLogin = document.getElementById('LoginInput');
+cfml.userPassword = document.getElementById('PasswordInput');
 
-cfml.authBT.addEventListener('click', function(event) {
+cfml.authBT.addEventListener('click', function(e) {
 	cfml.authForm.submit();
 })
